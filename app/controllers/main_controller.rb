3e5d7d5f9f98e114219d11b852b6afd31218bf3a
Scott Murphy <scmurphy2009@gmail.com>
2015-01-08 16:57:24 -0500

    removed old unused methods

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,16 +7,5 @@ class MainController < ApplicationController
   def new_user
     
   end
-
-  def application
-    
-  end
-
-  def create_user
-    UsersController.create(params)
-    respond_to do |format|
-      format.html { redirect_to home_path }
-    end
-  end
   
 end
