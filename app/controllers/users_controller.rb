4e4fdde3c48c65a973299902047c46aa878e03ae
Scott Murphy <scmurphy2009@gmail.com>
2015-01-09 15:22:38 -0500

    removed unused scaffolded methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,26 +8,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
   # POST /users
   # POST /users.json
   def create
@@ -39,20 +19,6 @@ class UsersController < ApplicationController
     user.save
     respond_to do |format|
       format.js { render :json => { :new_user => user } } 
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
     end
   end
 
